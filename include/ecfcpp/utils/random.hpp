d692bfe134c03acb35e0b5ea43976c66ada77b8a
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2018-12-12 01:27:59 +0100

    Added PCG library for optional usage

--- include/ecfcpp/utils/random.hpp
+++ include/ecfcpp/utils/random.hpp
@@ -1,6 +1,10 @@
 #ifndef ECFCPP_UTILS_RANDOM_HPP
 #define ECFCPP_UTILS_RANDOM_HPP
 
+#ifdef ECFCPP_USE_PCG
+#include <pcg_random.hpp>
+#endif
+
 #include <algorithm>
 #include <random>
 
@@ -10,20 +14,24 @@ namespace ecfcpp
 namespace random
 {
 
-static std::random_device randomDevice;
-static std::mt19937 randomGenerator{ randomDevice() };
+#ifdef ECFCPP_USE_PCG
+static pcg32 randomGenerator{ pcg_extras::seed_seq_from< std::random_device >{} };
+#else
+static std::mt19937 randomGenerator{ std::random_device{}() };
+#endif
+
 static std::uniform_real_distribution< double > uniformDistribution{ 0, 1 };
 static std::normal_distribution< double > normalDistribution{ 0, 1 };
 
 template< typename T = double >
-T uniform( T const lower = 0, T const upper = 1 ) noexcept
+inline T uniform( T const lower = 0, T const upper = 1 ) noexcept
 {
     auto const & [ min, max ] = std::minmax( lower, upper );
     return static_cast< T >( min + uniformDistribution( randomGenerator ) * ( max - min ) );
 }
 
 template< typename T = double >
-T normal( double const mean = 0, double const stddev = 1 ) noexcept
+inline T normal( double const mean = 0, double const stddev = 1 ) noexcept
 {
     return static_cast< T >( mean + stddev * normalDistribution( randomGenerator ) );
 }
